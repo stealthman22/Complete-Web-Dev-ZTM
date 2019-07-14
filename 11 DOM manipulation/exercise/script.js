04d2d6ccb11d89bff1e08cef0fa0f72e544c29dc
stealthman22 <tdotonline@gmail.com>
2019-07-14 02:02:49 +0000

    Working on building the to to list

--- 11 DOM manipulation/exercise/script.js
+++ 11 DOM manipulation/exercise/script.js
@@ -1,30 +1,66 @@
 var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
+var items = document.querySelectorAll('.list');
+
+
 
 function inputLength() {
-	return input.value.length;
+    return input.value.length;
 }
 
 function createListElement() {
-	var li = document.createElement("li");
-	li.appendChild(document.createTextNode(input.value));
-	ul.appendChild(li);
-	input.value = "";
+    var li = document.createElement("li");
+
+    // Adds the item class to dynamically created li
+    li.classList.add('list');
+
+    li.appendChild(document.createTextNode(input.value));
+    ul.appendChild(li);
+    input.value = "";
 }
 
 function addListAfterClick() {
-	if (inputLength() > 0) {
-		createListElement();
-	}
+    if (inputLength() > 0) {
+        createListElement();
+    }
 }
 
 function addListAfterKeypress(event) {
-	if (inputLength() > 0 && event.keyCode === 13) {
-		createListElement();
-	}
+    if (inputLength() > 0 && event.keyCode === 13) {
+        createListElement();
+    }
 }
+
+/* function hasClass(items, list) {
+    return items.classList.contains(list);
+
+} */
+
+
+
+/* function addClick() {
+    if (hasClass(e.target, 'list')) {
+        console.log()
+    }
+} */
+
+function toggleDone() {
+    console.log('click');
+    items.classList.add('done');
+
+
+    /* if (e === true) {
+        items = document.querySelectorAll('li').classList.add('done');
+    } else {
+        items = document.querySelectorAll('li').classList.remove('done');
+    } */
+}
+
 
 button.addEventListener("click", addListAfterClick);
 
 input.addEventListener("keypress", addListAfterKeypress);
+
+
+items.forEach(item => item.addEventListener('click', toggleDone));
