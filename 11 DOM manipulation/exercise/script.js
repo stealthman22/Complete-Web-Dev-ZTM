6dd517ba39bf8fd7a75d6900971767666d136ece
stealthman22 <tdotonline@gmail.com>
2019-07-15 15:43:04 +0000

    Solved the to-do list challenge halfway

--- 11 DOM manipulation/exercise/script.js
+++ 11 DOM manipulation/exercise/script.js
@@ -1,23 +1,47 @@
 var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
-var items = document.querySelectorAll('.list');
+var lists = document.querySelectorAll('.listMenu > li');
 
 
+// Create list buttons
+
+var btn = document.createElement('button');
+btn.appendChild(document.createTextNode('Del'));
+
+
+//lists.forEach(list => list.appendChild(btn));
+
+btn.classList.add('btn');
+
+
+console.log(btn)
 
 function inputLength() {
     return input.value.length;
 }
 
+
+
+
+
 function createListElement() {
     var li = document.createElement("li");
-
-    // Adds the item class to dynamically created li
-    li.classList.add('list');
-
     li.appendChild(document.createTextNode(input.value));
     ul.appendChild(li);
     input.value = "";
+    // Adds the item class to dynamically created li
+    li.classList.add('items');
+
+    li.appendChild(btn);
+
+    // Add event listener
+    li.addEventListener('click', () => {
+        li.classList.toggle('done');
+        console.log('click');
+    })
+
+
 }
 
 function addListAfterClick() {
@@ -32,35 +56,19 @@ function addListAfterKeypress(event) {
     }
 }
 
-/* function hasClass(items, list) {
-    return items.classList.contains(list);
-
-} */
-
-
-
-/* function addClick() {
-    if (hasClass(e.target, 'list')) {
-        console.log()
-    }
-} */
-
 function toggleDone() {
-    console.log('click');
-    items.classList.add('done');
-
-
-    /* if (e === true) {
-        items = document.querySelectorAll('li').classList.add('done');
-    } else {
-        items = document.querySelectorAll('li').classList.remove('done');
-    } */
+    console.log(this)
+    this.classList.toggle('done');
 }
+
+
+
+
+
 
 
 button.addEventListener("click", addListAfterClick);
 
 input.addEventListener("keypress", addListAfterKeypress);
 
-
-items.forEach(item => item.addEventListener('click', toggleDone));
+lists.forEach(list => list.addEventListener('click', toggleDone))
