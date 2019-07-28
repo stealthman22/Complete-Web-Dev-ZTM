041b239bb7f16e88385c7ef96de52e515abcdbf8
stealthman22 <tdotonline@gmail.com>
2019-07-28 04:17:22 +0000

    Finished Es8. Worked through ref and value, Type coercion,  ES7

--- 12 Advanced JavaScript/ES7.js
+++ 12 Advanced JavaScript/ES7.js
@@ -2,6 +2,14 @@
 const pets = ['cat', 'dog', 'bat'];
 pets.includes('dog');
 
+const string = 'letter';
+string.includes('e');
+
+const obj = {
+    a: 'test',
+}
+obj.includes(test);
+
 //Exponential operator
 const square = x => x ** 2;
 const cube = y => y ** 3;
---
+++ 12 Advanced JavaScript/ES8.js
@@ -0,0 +1,45 @@
+// String padding
+'Turtle'.padStart();
+
+'Turtle'.padEnd();
+
+
+// Trailing commas
+const fun = (a, b, c, d, ) => {
+    console.log(a);
+}
+
+fun(1, 2, 3, 4, );
+
+// Object.values  Object.entries;
+
+let obj = {
+    user0: 'Santa',
+    user1: 'Rogue',
+    user2: 'Baller'
+}
+
+// Old approach
+// To treat objects like arrays, looping over them
+Object.keys(obj).forEach((key, index) => {
+    console.log(key, obj[key]);
+})
+
+
+Object.values(obj).forEach(value => {
+    console.log(value);
+})
+
+Object.entries(obj).forEach(value => {
+    console.log(value);
+})
+
+
+// Replacing the content of the key forthe items 
+//   user0: 'Santa', becomes Santa0
+Object.entries(obj).map(value => {
+    return value[1] + value[0].replace('user', '');
+})
+
+
+// Async Await
