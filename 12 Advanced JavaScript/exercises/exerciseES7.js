5706e189c1d61300010025d44e08492e7ec2d058
stealthman22 <tdotonline@gmail.com>
2019-08-01 23:59:09 +0000

    init commit

--- 12 Advanced JavaScript/exercises/exerciseES7.js
+++ 12 Advanced JavaScript/exercises/exerciseES7.js
@@ -4,6 +4,8 @@
 // #1) Check if this array includes the name "John".
 const dragons = ['Tim', 'Johnathan', 'Sandy', 'Sarah'];
 
+// sol
+dragons.includes('John');
 
 // #2) Check if this array includes any name that has "John" inside of it. If it does, return that
 // name or names in an array.
@@ -15,4 +17,3 @@ const dragons = ['Tim', 'Johnathan', 'Sandy', 'Sarah'];
 
 // #4) Useing your function from #3, put in the paramter 10000. What is the result?
 // Research for yourself why you get this result
-
