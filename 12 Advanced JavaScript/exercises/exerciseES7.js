09b35388fd6901fe00dcb46d59ffd818a2c1bca7
stealthman22 <tdotonline@gmail.com>
2019-08-02 01:32:59 +0000

    Done with exercises for ES7

--- 12 Advanced JavaScript/exercises/exerciseES7.js
+++ 12 Advanced JavaScript/exercises/exerciseES7.js
@@ -11,9 +11,38 @@ dragons.includes('John');
 // name or names in an array.
 const dragons = ['Tim', 'Johnathan', 'Sandy', 'Sarah'];
 
+// sol
+const filterDragons = dragons.filter(dragon => dragon.includes('John'));
+console.log(filterDragons);
+
 
 // #3) Create a function that calulates the power of 100 of a number entered as a parameter
 
+// sol
+function exponent(num) {
+    return num ** 100
+}
 
-// #4) Useing your function from #3, put in the paramter 10000. What is the result?
+
+
+// #4) Using your function from #3, put in the paramter 10000. What is the result?
+
+// sol
+exponent(1000)
+    // 1e+300
+
+
 // Research for yourself why you get this result
+
+/* 
+It is called exponential notation. And the computer does this to save display space and storage. 
+1 is the mantissa
++300 is the exponent.
+This simply means 1 to the power of 300
+*/
+
+// Was supposed to be 
+
+exponent(10000);
+
+// Infinity
