8753708d0761523f704d197d48022214796b0a25
stealthman22 <tdotonline@gmail.com>
2019-10-07 03:38:04 +0000

    Building the express server

---
+++ 24Nodejs/express.js
@@ -0,0 +1,18 @@
+// Building express server
+
+const express = require('express'),
+
+const app = express();
+
+
+
+// Get request with express
+app.get('/', (req, res) => {
+    const user {
+        name: 'Sally',
+        hobby: 'Soccer'
+   }
+    res.send(user)
+});
+app.listen(3000);
+
--- 24Nodejs/server.js
+++ 24Nodejs/server.js
@@ -1,7 +1,17 @@
 const http = require('http');
 
-const server = http.createServer(() => {
-    console.log('I hear you! Thanks for the request:)')
+const server = http.createServer((request, response) => {
+    // console.log('headers', request.headers)
+    console.log('method', request.method)
+    console.log('url', request.url)
+    const user = {
+        name: 'John',
+        hobby: 'Skating'
+    }
+
+    // Sertting json as the content type
+    response.setHeader('Content-Type', 'application/json');
+    response.end(JSON.stringify(user));
 })
 
 server.listen(3000);
