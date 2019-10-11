73d60ffb3fc51500fb2a369f0b1f1554f5326f02
stealthman22 <tdotonline@gmail.com>
2019-10-11 05:03:07 +0000

    Working on advent code challlenge

---
+++ 24Nodejs/awesome node/advent.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+
+/* const advent = fs.readFile('./input.txt', (err, data) => {
+    if (err) {
+        console.log('You are doing it wrong')
+    } console.log(data.toString())
+}) */
+
+// House the function into a variable
+const advent = fs.readFileSync('./input.txt').toString();
+
+// Spread the results of function above into an array
+// Only worked with readfielsync, can't explain why.
+const adventArray = [...advent];
+
+/* const sumAdvent = adventArray.reduce((param, acc) => {
+    for (let i = 0; i < adventArray.length; i++) {
+        if (param === '(') {
+            acc + 1;
+        } else { acc - 1 };
+
+    }
+    return adventArray
+}, 0) */
+
+const sumAdvent = adventArray.map(param => {
+    if (param === '(') {
+        param + 1;
+    }
+    ;
+})
+
+console.log(sumAdvent);
--- 24Nodejs/awesome node/script.js
+++ 24Nodejs/awesome node/script.js
@@ -1,8 +1,36 @@
 const fs = require('fs');
 
+// Read
 fs.readFile('./hello.txt', (err, data) => {
     if (err) {
         console.log('errrrorrrr');
     }
-    console.log(data.toString());
+    console.log('async', data.toString('utf8'));
 });
+
+const file = fs.readFileSync('./hello.txt').toString();
+console.log('sync', file);
+
+// Append
+fs.appendFile('./hello.txt', ' This is so cooool', err => {
+    if (err) {
+        console.log(err)
+    }
+})
+
+// Write
+fs.writeFile('bye.txt', 'Sad to see you go', err => {
+    if (err) {
+        console.log('sad to see you go dear')
+    }
+})
+
+// Delete
+fs.unlink('./bye.txt', err => {
+    if (err) {
+        console.log(err)
+    }
+    console.log('Inception')
+
+})
+
