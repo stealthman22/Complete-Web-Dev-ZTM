cbaa2e71d6e7a5a8d5099c81176a6be966705c28
stealthman22 <tdotonline@gmail.com>
2019-10-12 23:29:33 +0000

    Still trying to solve code challenge

---
+++ 24Nodejs/awesome node/advent2.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+
+// Read file synchronously
+const advent = fs.readFileSync('./input.txt').toString()
+
+// Spread file into an array
+const adventArray = [...advent]
+
+
+const sumOfAdventArray = adventArray.filter(caract => {
+
+
+})
