995fb8655095156817163456199eb0f61f5f11de
stealthman22 <tdotonline@gmail.com>
2019-10-12 23:26:48 +0000

    Few manipulations, revositing the knowledge

--- 12 Advanced JavaScript/advArrays.js
+++ 12 Advanced JavaScript/advArrays.js
@@ -1,35 +1,35 @@
-/* // Push results of forEach into a new array
+ // Push results of forEach into a new array
 
-const array = [1, 2, 3, 10, 16];
-// Placeholder array
-const double = []
-const newArray = array.forEach((num) => {
-    double.push(num * 2);
-})
-console.table('forEach', double);
+ const array = [1, 2, 3, 10, 16];
+ // Placeholder array
+ const double = []
+ const newArray = array.forEach((num) => {
+     double.push(num * 2);
+ })
+ console.table('forEach', double);
 
 
-// map, filter, reduce
+ // map, filter, reduce
 
-//const mapArray = array.map((num) => {
-//  return num * 2;
-//});
+ //const mapArray = array.map((num) => {
+ //  return num * 2;
+ //});
 
-const mapArray = array.map(num => num + 2);
+ const mapArray = array.map(num => num + 2);
 
-console.table('map', mapArray);
+ console.table('map', mapArray);
 
-// filter
+ // filter
 
-const filterArray = array.filter(num => num > 5);
-console.table('filter', filterArray); */
+ const filterArray = array.filter(num => num > 5);
+ console.table('filter', filterArray);
 
-const array = [1, 2, 3, 10, 16];
-// reduce
-// takes in an accumulator and then the param
-const reduceArray = array.reduce((accumulator, num) => {
-    return accumulator + num
+ const array = [1, 2, 3, 10, 16];
+ // reduce
+ // takes in an accumulator and then the param
+ const reduceArray = array.reduce((accumulator, num) => {
+     return accumulator + num
 
-}, 7);
+ }, 7);
 
-console.table('reduce', reduceArray);
+ console.table('reduce', reduceArray);
