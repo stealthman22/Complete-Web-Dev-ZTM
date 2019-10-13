c227664cf239fce66e5a179ae774cac93d47acb8
stealthman22 <tdotonline@gmail.com>
2019-10-13 11:47:57 +0000

    Cracked the code I got it working

--- 24Nodejs/awesome node/advent.js
+++ 24Nodejs/awesome node/advent.js
@@ -1,32 +1,33 @@
 const fs = require('fs');
 
-const advent = fs.readFile('./input.txt', (err, data) => {
+/* const advent = fs.readFile('./input.txt', (err, data) => {
     console.time('elapse')
     if (err) {
         console.log('You are doing it wrong')
     }
     console.log(data.toString())
     console.timeEnd('elapse')
-})
+}) */
 
 // House the function into a variable
-//const advent = fs.readFileSync('./input.txt').toString();
+const advent = fs.readFileSync('./input.txt').toString();
+//console.log(advent);
 
 // Spread the results of function above into an array
 // Only worked with readfielsync, can't explain why.
-//const adventArray = [...advent];
+const adventArray = [...advent];
+//console.log(adventArray);
 
-/* const sumAdvent = adventArray.reduce((param, acc) => {
-    for (let i = 0; i < adventArray.length; i++) {
-        if (param === '(') {
-            acc + 1;
-        } else {
-            acc - 1
-        };
+const sumAdvent = adventArray.reduce((acc, param) => {
+    if (param === '(') {
+        return acc + 1;
+    } else {
+        return acc - 1
+    };
 
-    }
-    return adventArray
-}, 0) */
+}, 0)
+
+console.log(sumAdvent)
 
 /* const sumAdvent = adventArray.map(param => {
     if (param === '(') {
