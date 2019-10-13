c9986d48f99a7143b48bce13e6507fb3388ebc4c
stealthman22 <tdotonline@gmail.com>
2019-10-13 02:09:17 +0000

    Added the console.time functionality

--- 24Nodejs/awesome node/advent.js
+++ 24Nodejs/awesome node/advent.js
@@ -1,23 +1,28 @@
 const fs = require('fs');
 
-/* const advent = fs.readFile('./input.txt', (err, data) => {
+const advent = fs.readFile('./input.txt', (err, data) => {
+    console.time('elapse')
     if (err) {
         console.log('You are doing it wrong')
-    } console.log(data.toString())
-}) */
+    }
+    console.log(data.toString())
+    console.timeEnd('elapse')
+})
 
 // House the function into a variable
-const advent = fs.readFileSync('./input.txt').toString();
+//const advent = fs.readFileSync('./input.txt').toString();
 
 // Spread the results of function above into an array
 // Only worked with readfielsync, can't explain why.
-const adventArray = [...advent];
+//const adventArray = [...advent];
 
 /* const sumAdvent = adventArray.reduce((param, acc) => {
     for (let i = 0; i < adventArray.length; i++) {
         if (param === '(') {
             acc + 1;
-        } else { acc - 1 };
+        } else {
+            acc - 1
+        };
 
     }
     return adventArray
