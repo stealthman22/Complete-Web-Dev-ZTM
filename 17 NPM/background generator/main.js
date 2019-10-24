1f2dd2b4cd5bb43d6a9c18d161051440a4d8fa1f
stealthman22 <tdotonline@gmail.com>
2019-10-24 22:39:53 +0000

    Added few comments

--- 17 NPM/background generator/main.js
+++ 17 NPM/background generator/main.js
@@ -1,3 +1,5 @@
+// Lodash has a lot of ectended functionality, but a lot of boiler plates too
+
 var _ = require('lodash');
 
 var array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
@@ -29,6 +31,6 @@ color1.addEventListener('input', setGradient);
 color2.addEventListener('input', setGradient);
 
 
-/* Why does the printed linear gradient have a , at the end. 
-    When do we use textContent 
+/* Why does the printed linear gradient have a , at the end.
+    When do we use textContent
 */
