667585da9b22f499b2516ab5a7389b86e412e2a1
stealthman22 <tdotonline@gmail.com>
2019-11-08 10:46:51 +0000

    Few edits

--- 24Nodejs/Env Var/server.js
+++ 24Nodejs/Env Var/server.js
@@ -2,10 +2,17 @@ const app = require('http')
     .createServer((req, res) => res.sendDate('hey there I am active'));
 
 // congigure with env variables dynamically
-const PORT = process.DATABASE_URL
+const PORT = process.env.PORT
 app.listen(process.env.PORT, () => {
-    console.log(`Server is up on ${DATABASE_URL} !!`)
+    console.log(`Server is up on ${PORT} !!`)
 })
 
+console.log(PORT)
+
+
+
+// congigure with env variables dynamically
+const DATABASE_URL = process.env.DATABASE_URL
+
 // env variables
-console.log(PORT);
+console.log(DATABASE_URL);
